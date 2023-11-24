da55a0056a0dcb02decb2bb7c1b8e1b7e1f067e6
Lê Bá Mùi <lb_mui@brycen.com.vn>
2023-11-24 08:25:06 +0700

    test spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularNgrxBaseLine'`, () => {
+  it(`should have as title 'angular-ngrx-base-line'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularNgrxBaseLine');
+    expect(app.title).toEqual('angular-ngrx-base-line');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('AngularNgrxBaseLine app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-ngrx-base-line app is running!');
   });
 });
